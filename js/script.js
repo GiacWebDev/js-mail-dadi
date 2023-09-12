0d76e836731482983e9db9fc13b440614d964fe4
GiacWebDev <giacobechini@gmail.com>
2023-09-12 17:08:36 +0200

    [edit] stamp on page mail message

--- js/script.js
+++ js/script.js
@@ -4,33 +4,39 @@
 // creo le variabili
 const inputEl = document.querySelector('input');
 const btnMailEl = document.querySelector('button');
+const messaggioEl = document.querySelector('.messaggio')
 
 // creo lista array per mail che possono accedere
 
-const mailValide = ['ciao@comestai.it', 'ciao1@comestai.it', 'ciao2@comestai.it',]
+const mailValide = ['ciao@comestai.it', 'ciao1@comestai.it', 'ciao2@comestai.it']
 
 // scateno il pulsante Verifica
 btnMailEl.addEventListener('click', function() {
-
+  
   // ottengo il valore inserito nell'imput
   const inputMail = inputEl.value
 
+  
+  
   // faccio un ciclo sulla lista delle mail per verificare se sono uguali a mail valide
   for (let i = 0; i < mailValide.length; i++) {
 
+    messaggioEl.innerHTML = '';
+    
     if (inputMail === mailValide[i]) {
-      // se hai trovato la corrispondenza imposta l'accesso valido
-      accessoConsentito = true;
       console.log('accesso eseguito');
+      messaggioEl.innerHTML = 'accesso eseguito';
       // esco dal ciclo quando trova una corrisponzenza
       break;
       
-    
     } else if (inputMail !== mailValide[i]) {
       console.log('accesso negato');
+      messaggioEl.innerHTML = 'accesso negato';
     }
   }
 });
 
 
 
+
+// ########################## DADI ###########################
