6203b3ff7fcfce2eeec4572883640c9efcff8dde
GiacWebDev <giacobechini@gmail.com>
2023-09-12 15:08:06 +0200

    [edit] js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+
+// creo le variabili
+const inputEl = document.querySelector('.email');
+const btnMail = document.querySelector('.bnt-mail');
+
+// creo lista array per mail che possono accedere
+const mailValide = ['ciao@comestai.it', 'ciao1@comestai.it', 'ciao2@comestai.it',]
+
+// scateno il pulsante Verifica
+btnMail.addEventListener('click', function() {
+  console.log('ciao');
+
+});
